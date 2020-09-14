2dc95349fcfcf6a8b21eac0c3fb441367c439c75
cheretick <55267049+cheretick@users.noreply.github.com>
2020-09-14 18:46:29 -0400

    Initial commit

---
+++ HW3/NameAndAddress/NameAndAddress/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace NameAndAddress
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Enter your name: ");
+			string name = Console.ReadLine();
+			Console.Write("Enter your address: ");
+			string address = Console.ReadLine();
+
+			StreamWriter sw = new StreamWriter("G:\\C#\\.NET Projects\\CSET3200\\HW3\\NameAndAddress\\NameAndAddress\\Properties\\NameAndAddress.txt");
+			sw.WriteLine($"Student Name: {name}");
+			sw.WriteLine($"Student Address: {address}");
+			sw.Close();
+		}
+	}
+}
