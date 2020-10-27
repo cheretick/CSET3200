9131955fb9f983769c79df691186a8d73115ec5b
cheretick <55267049+cheretick@users.noreply.github.com>
2020-10-27 18:54:48 -0400

    Completed Homework 9

---
+++ HW9/Homework 9/Homework 9/wwwroot/JavaScript/JavaScript.js
@@ -0,0 +1,19 @@
+﻿var username = "cheretick";
+var password = "password";
+
+function checkLogin() {
+	var enteredUsername = document.getElementById("username").value;
+	var enteredPassword = document.getElementById("password").value;
+
+	if ((username == enteredUsername) && (password == enteredPassword)) {
+		alert("Login successful!");
+		redirect();
+		return false;
+	} else {
+		alert("Invalid credentials. Please, try again.");
+	}
+}
+
+function redirect() {
+	window.location = "index.html";
+}
